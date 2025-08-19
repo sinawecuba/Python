69d939495394af59593fc7139c9a1300ef9d35c5
sinawecuba <sinawesome@gmail.com>
2025-08-19 17:36:50 +0200

    Add files via upload

---
+++ chapter 8/pets.py
@@ -0,0 +1,13 @@
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet('hamster', 'harry')
+describe_pet('dog', 'willie')
+
+describe_pet(animal_type='hamster', pet_name='harry')
+describe_pet(pet_name='harry', animal_type='hamster')
+
+describe_pet('dog', 'willie')  
+describe_pet(pet_name='henry', animal_type='pig')
---
+++ chapter 8/pizza.py
@@ -0,0 +1,29 @@
+# def make_pizza(*toppings):
+#     """Print the list of toppings that have been requested."""
+#     print(toppings)
+
+# make_pizza('pepperoni')
+# make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+# replacing print with for loop
+# def make_pizza(*toppings):
+#     """Summarize the pizza we are about to make."""
+#     print("\nMaking a pizza with the following toppings:")
+#     for topping in toppings:
+#         print(f"- {topping}")
+
+
+# # Calling the function with different numbers of toppings
+# make_pizza('pepperoni')
+# make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+#mxing positional and arbitrary arguments
+def make_pizza(size, *toppings):
+    """Summarize the pizza we are about to make."""
+    print(f"\nMaking a {size}-inch pizza with the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
